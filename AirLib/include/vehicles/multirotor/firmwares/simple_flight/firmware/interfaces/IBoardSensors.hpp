73ae4b29d0fff724d47ed193c364b13a6db0cdfa
Subedi Suman <Subedi@iabg.de>
2021-11-07 11:28:11 +0100

    implement interface to access sensor data from const board_, sensor models come from const vehicle_params_

--- AirLib/include/vehicles/multirotor/firmwares/simple_flight/firmware/interfaces/IBoardSensors.hpp
+++ AirLib/include/vehicles/multirotor/firmwares/simple_flight/firmware/interfaces/IBoardSensors.hpp
@@ -10,5 +10,10 @@ public:
     virtual void readGyro(float gyro[3]) const = 0; //angular velocity vector rad/sec
 
     virtual ~IBoardSensors() = default;
+    // // added by Suman
+    // virtual void readImuData(float accel[3], float gyro[3]) const = 0;
+    // virtual void readBarometerData(float* altitude) const = 0;
+    // virtual void readMagnetometerData(float mag[3]) const = 0;
+    // virtual void readGpsData(float geo[3], float vel[3]) const = 0;
 };
 }
