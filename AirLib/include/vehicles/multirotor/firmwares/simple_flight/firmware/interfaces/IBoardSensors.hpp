b87824e38f70ff42ca15de257e10f4c12909526b
Subedi Suman <Subedi@iabg.de>
2021-11-10 22:24:57 +0100

    fix board_ methods' polymorphism

--- AirLib/include/vehicles/multirotor/firmwares/simple_flight/firmware/interfaces/IBoardSensors.hpp
+++ AirLib/include/vehicles/multirotor/firmwares/simple_flight/firmware/interfaces/IBoardSensors.hpp
@@ -10,10 +10,14 @@ public:
     virtual void readGyro(float gyro[3]) const = 0; //angular velocity vector rad/sec
 
     virtual ~IBoardSensors() = default;
-    // // added by Suman
-    // virtual void readImuData(float accel[3], float gyro[3]) const = 0;
-    // virtual void readBarometerData(float* altitude) const = 0;
-    // virtual void readMagnetometerData(float mag[3]) const = 0;
-    // virtual void readGpsData(float geo[3], float vel[3]) const = 0;
+
+    virtual bool checkImuIfNew() const = 0;
+    virtual bool checkBarometerIfNew() const = 0;
+    virtual bool checkMagnetometerIfNew() const = 0;
+    virtual bool checkGpsIfNew() const = 0;
+    virtual void readImuData(real_T accel[3], real_T gyro[3]) const = 0;
+    virtual void readBarometerData(real_T* altitude) const = 0;
+    virtual void readMagnetometerData(real_T mag[3]) const = 0;
+    virtual void readGpsData(double geo[3], real_T vel[3]) const = 0;
 };
 }
